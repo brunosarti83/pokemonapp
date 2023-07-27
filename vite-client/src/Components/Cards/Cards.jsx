0cc093bee47cc56bfb3bcdcbe09025ef76052599
brunosarti83 <brunosarti.bs@gmail.com>
2023-07-27 17:59:41 -0300

    Cards button Next Back

--- vite-client/src/Components/Cards/Cards.jsx
+++ vite-client/src/Components/Cards/Cards.jsx
@@ -5,6 +5,9 @@ import { useSelector, useDispatch } from 'react-redux';
 import Card from '../Card/Card';
 // actions
 import { getPokemons } from '../../redux/actions';
+// styles
+import styles from './Cards.module.css';
+
 
 const Cards = () => {
 
@@ -13,22 +16,32 @@ const Cards = () => {
     const [nowShowing, setNowShowing] = useState([])
     const dispatch = useDispatch()
 
+    const pokemonsPerPage = 12
+
     useEffect(() => {
         dispatch(getPokemons())
     }, [])
 
     useEffect(() => {
-        const indexFrom = pag - 1 // included
-        const indexTo = ((allPokemons.length - 1) > (pag + 11)) ? (pag + 11) : (allPokemons.length - 1) // excluded
+        const indexTo = (pokemonsPerPage * pag) - 1
+        const indexFrom = indexTo - 11
         const thisPagePokemons = allPokemons.slice(indexFrom, indexTo)
         setNowShowing(thisPagePokemons)
     }, [pag, allPokemons])
+
+    const previousPage = () => {
+        setPag(pag-1)
+    } 
+
+    const nextPage = () => {
+        setPag(pag+1)
+    } 
 
     return (
         <div>
             <h1>This is Cards</h1>
             <div>
-                {nowShowing.map(pokemon => {
+                { nowShowing.map(pokemon => {
                     return <Card key={pokemon.id}
                         id={pokemon.id}
                         name={pokemon.name}
@@ -36,6 +49,10 @@ const Cards = () => {
                         types={pokemon.Types} />
                 })
                 }
+            </div>
+            <div className={styles.buttons}>
+                <div className={(pag > 1) ? styles.backButton : styles.noShow} onClick={previousPage}>Back</div>
+                <div className={(pag * pokemonsPerPage < allPokemons.length) ? styles.nextButton : styles.noShow} onClick={nextPage}>Next</div>
             </div>
         </div>
     )
