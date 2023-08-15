97bba7bc057d3964ba09d452fd307e03621abcbf
brunosarti83 <brunosarti.bs@gmail.com>
2023-08-15 16:58:02 -0300

    downgraded server 200 pokemons

--- api_v2/src/api_requests/getApiAllPokemons.js
+++ api_v2/src/api_requests/getApiAllPokemons.js
@@ -6,7 +6,7 @@ const formatForCards = require('./formatForCards');
 const getApiAllPokemons = async () => {
     try {
         let requests = []
-        let next = `https://pokeapi.co/api/v2/pokemon?limit=400`  
+        let next = `https://pokeapi.co/api/v2/pokemon?limit=200`  
         //while (next) {
             const { data } = await axios.get(next)
             const { results } = data
