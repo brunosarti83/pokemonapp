5d716fbc9bfe82458988c149b2c1aebfd7371925
brunosarti83 <brunosarti.bs@gmail.com>
2023-08-16 19:52:36 -0300

    fixed too large variable font on Detail

--- vite-client/src/helpers/nameSizeSetter.js
+++ vite-client/src/helpers/nameSizeSetter.js
@@ -1,6 +1,6 @@
 
 export default (name) => {
-    const sizeVar = ((name.length - 10) * -2) + 16
+    const sizeVar = ((name.length - 10) * -2) + 14
     const sizeFix = ((750 / name.length) * 1.6) 
     document.documentElement.style.setProperty('--name-size-var', `${sizeVar}dvw`)
     document.documentElement.style.setProperty('--name-size-fix', `${sizeFix}px`)
