2ffe764c6b87ed0ecf8dfb1132729971f8ac1571
brunosarti83 <brunosarti.bs@gmail.com>
2023-08-25 17:00:02 -0300

    working on filters and order inside getDBAllPokemons

--- api_v2/src/database_requests/getDBAllPokemons.js
+++ api_v2/src/database_requests/getDBAllPokemons.js
@@ -1,22 +1,42 @@
+const { Op } = require('sequelize')
 const { Type, Pokemon } = require('../db');
 
-const getDBAllPokemons = async (limit, offset) => {
+const getDBAllPokemons = async (limit, offset, filterObj) => {
+
+    const { type, origin, orderBy, direction } = filterObj
+    const where = {}
+    const includeWhere = {}
+    if (type) { includeWhere.name = type }
+    switch (origin) {
+        case 'original':
+            where.api_id = { [Op.not]: null }
+            break
+        case 'custom':
+            where.api_id = { [Op.is]: null }
+            break
+        default:
+            break
+    }
+    
 
     try {
-        const count = await Pokemon.count()
-        const results = await Pokemon.findAll({
+        const results = await Pokemon.findAndCountAll({
             limit, 
             offset,
+            where,
             attributes: ['id', 'api_id','name', 'image', 'attack'],
             include: {
                 model: Type,
+                where: includeWhere,
                 attributes: ['name'],
                 through: {
                     attributes: []
                 }
-            }
+            },
+            distinct: true
         })
-        return { count, limit, offset, results }
+         
+        return { limit, offset, ...results }
         
     } catch (error) {
         throw error
